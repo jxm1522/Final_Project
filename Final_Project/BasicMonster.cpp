5b2688af52992769c252f614e8b3dfd345d60c68
ara1123 <45371899+ara1123@users.noreply.github.com>
2018-12-01 14:44:20 -0500

    Add files via upload

---
+++ Final_Project/BasicMonster.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include "BasicMonsterClass.h"
+using namespace std;
+
---
+++ Final_Project/BasicMonster.h
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class BasicMonster
+{
+private: 
+	int health_points = 0;
+	int exp_given = 0;
+
+public:
+
+	attack(Character Char1);
+};
