6f5e1aee811ba3e2cce6dff622c881621b9ada23
ara1123 <45371899+ara1123@users.noreply.github.com>
2018-12-02 20:11:00 -0500

    Add files via upload

---
+++ Final_Project/BasicMonster.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "BasicMonster.h"
+using namespace std;
+
+
+void BasicMonster::levelup(void)
+{
+	level += 1;
+}
+
+int BasicMonster::gethealth(void)
+{
+	return health_points;
+}
+
+void BasicMonster::sethealth(int x)
+{
+	health_points = x;
+}
+
+int BasicMonster::getexperience(void)
+{
+	return experience;
+}
+
+void BasicMonster::setexperience(int x)
+{
+	experience = x;
+}
+
+int BasicMonster::getlevel(void)
+{
+	return level;
+}
+
+void BasicMonster::setlevel(int x)
+{
+	level = x;
+}
---
+++ Final_Project/BasicMonster.h
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class BasicMonster
+{
+private:
+	int health_points, experience, level;
+
+public:
+
+	// Setters and Getters
+	void levelup(void);
+	int gethealth(void);
+	void sethealth(int x);
+	int getexperience(void);
+	void setexperience(int x);
+	int getlevel(void);
+	void setlevel(int x);
+
+};
