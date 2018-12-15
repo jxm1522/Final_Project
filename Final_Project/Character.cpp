6965056ca926235592e0385be0a21a46896f7f02
John McCormick <jxm1522@rit.edu>
2018-12-15 15:38:37 -0500

    Fixed fight function in Character.cpp

--- Final_Project/Character.cpp
+++ Final_Project/Character.cpp
@@ -47,10 +47,10 @@ int Character::doDamage()
 void Character::fight(BasicMonster &M)
 {
 	int temp, enemy_damage;
+	;
+	enemy_damage = 10 * level;
 
-	enemy_damage = 10 * (M.getlevel);
-
-	temp = M.gethealth - this->damage;
+	temp = health_points - this->damage;
 	M.sethealth(temp);
 
 	health_points = this->health_points - enemy_damage;
