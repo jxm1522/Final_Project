aab901c4b1248cbf17a473649b107d2b17b0d75f
John McCormick <jxm1522@rit.edu>
2018-12-16 23:41:39 -0500

    added finite number of health potions

--- Final_Project/Interaction.cpp
+++ Final_Project/Interaction.cpp
@@ -3,79 +3,83 @@
 
 using namespace std;
 
-void fight(vector<class Character> &charList, vector<class BasicMonster> &monstList) { // implementing fight class
+void fight(vector<class Character> &charList, vector<class BasicMonster> &monstList, int &healthPotionNum) { // implementing fight class
 	int characterChoice, monsterChoice, playerChoice;
 
 	while (checkTeam(charList, monstList) == 2) {
-			for (int i = 0; i < charList.size(); i++) { //Character side attacks
-				cout << "-------------------------------" << endl;
-				for (int j = 0; j < monstList.size(); j++) {			//Lists Monster stats
-					int temp = j + 1;
-					cout << monstList[j].getName() << " " << temp << " has " << monstList[j].gethealth() << " health" << endl;
-				}
-				cout << "-------------------------------" << endl;
-				cout << "-------------------------------" << endl;
-
-				for (int j = 0; j < charList.size(); j++) {			//Lists Player stats
-
+		for (int i = 0; i < charList.size(); i++) { //Character side attacks
+			cout << "-------------------------------" << endl;
+			for (int j = 0; j < monstList.size(); j++) {			//Lists Monster stats
+				int temp = j + 1;
+				cout << monstList[j].getName() << " " << temp << " has " << monstList[j].gethealth() << " health" << endl;
+			}
+			cout << "-------------------------------" << endl;
+			cout << "-------------------------------" << endl;
+			for (int j = 0; j < charList.size(); j++) {			//Lists Player stats
 					cout << charList[j].getName() << " " << " has " << charList[j].getHealth() << " health" << endl;
-				}
-				cout << "-------------------------------" << endl << endl;
-
+			}
+			cout << "Health potions available: " << healthPotionNum << endl;
+			cout << "-------------------------------" << endl << endl;
 				cout << "Which character would you like to interact with? " << endl; //Character Selection to attack
-				for (int j = 1; j <= charList.size(); j++) {
-					cout << "(" << j << ") : " << charList[(j - 1)].getName() << endl;
-				}
+			for (int j = 1; j <= charList.size(); j++) {
+				cout << "(" << j << ") : " << charList[(j - 1)].getName() << endl;
+			}
+			cin >> characterChoice;
+			--characterChoice;
+
+			while (charList[characterChoice].getHealth() <= 0 && charList[characterChoice].getAttackStatus() == true) { //Makes sure character choice is valid
+
+				cout << "Please choose a character that hasn't healed or attacked (You may not interact with the same character twice in one turn)" << endl;
+				cout << endl << "Which character would you like to interact with? " << endl;
 				cin >> characterChoice;
 				--characterChoice;
+			}
 
-				while (charList[characterChoice].getHealth() <= 0 || charList[characterChoice].getAttackStatus() == true) { //Makes sure character choice is valid
+			cout << "Would you like to heal, or attack?" << endl;
+			cout << "(1) Attack" << endl;
+			cout << "(2) Heal" << endl;
+			cin >> playerChoice;
 
-					cout << "Please choose a character that hasn't healed or attacked (You may not interact with the same character twice in one turn)" << endl;
-					cout << endl << "Which character would you like to interact with? " << endl;
-					cin >> characterChoice;
-					--characterChoice;
-
+			if (playerChoice == 2) {			//Heal
+				
+				if (healthPotionNum == 0) {
+					playerChoice = 1;
+					cout << "Looks like you're all out of health potions!  You'll be attacking with this character instead." << endl;
 				}
-
-				cout << "Would you like to heal, or attack?" << endl;
-				cout << "(1) Attack" << endl;
-				cout << "(2) Heal" << endl;
-				cin >> playerChoice;
-
-				if (playerChoice == 2) {			//Heal
-
+				else {
+					healthPotionNum--;
 					charList[characterChoice]++;
 					charList[characterChoice].setAttackStatus(true);
 				}
-				else if (playerChoice == 1) {			//Attack
-
-					do {
-
-						cout << endl << "Which monster would you like to attack? Choose between 1 and " << monstList.size() << ": "; //Monster selection to be attacked
-						cin >> monsterChoice;
-						--monsterChoice;
-						if (monstList[monsterChoice].checkifDead()) {
-							cout << "Please choose a monster that is alive" << endl;
-						}
-					} while (monstList[monsterChoice].checkifDead());
-
-
-
-					int missChar = rand() % 101; //Chance for character to miss
-					if (missChar > 85) {
-						cout << charList[characterChoice].getName() << " has missed!" << endl;
-						charList[characterChoice].setAttackStatus(true);
-					}
-					else {
-						int damage = monstList[monsterChoice].gethealth() - charList[characterChoice].damageVal();
-						monstList[monsterChoice].sethealth(damage);
-						charList[characterChoice].setAttackStatus(true);
-						cout << endl << charList[characterChoice].getName() << " attacks! " << endl;
-						cout << charList[characterChoice].getName() << " does " << charList[characterChoice].damageVal() << " damage" << endl << endl;
-					}
-				}			//End Attack
 			}
+			else if (playerChoice == 1) {			//Attack
+
+				do {
+
+					cout << endl << "Which monster would you like to attack? Choose between 1 and " << monstList.size() << ": "; //Monster selection to be attacked
+					cin >> monsterChoice;
+					--monsterChoice;
+					if (monstList[monsterChoice].checkifDead()) {
+						cout << "Please choose a monster that is alive" << endl;
+					}
+				} while (monstList[monsterChoice].checkifDead());
+
+
+
+				int missChar = rand() % 101; //Chance for character to miss
+				if (missChar > 85) {
+					cout << charList[characterChoice].getName() << " has missed!" << endl;
+					charList[characterChoice].setAttackStatus(true);
+				}
+				else {
+					int damage = monstList[monsterChoice].gethealth() - charList[characterChoice].damageVal();
+					monstList[monsterChoice].sethealth(damage);
+					charList[characterChoice].setAttackStatus(true);
+					cout << endl << charList[characterChoice].getName() << " attacks! " << endl;
+					cout << charList[characterChoice].getName() << " does " << charList[characterChoice].damageVal() << " damage" << endl << endl;
+				}
+			}			//End Attack
+		}
 
 		 
 		for (int i = 0; i < monstList.size(); i++) {		// Monster side attacks
--- Final_Project/Interaction.h
+++ Final_Project/Interaction.h
@@ -12,5 +12,5 @@
 #include <string>
 #include <vector>
 
-void fight(vector<class Character> &charList, vector<class BasicMonster> &monstList);  
+void fight(vector<class Character> &charList, vector<class BasicMonster> &monstList, int &);  
 int checkTeam(vector<class Character> &charList, vector<class BasicMonster> &monstList); //Checks if whole team of characters or monsters is dead 
